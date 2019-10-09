be851a7b5b89f38318aa94dd284e680a11229ae3
Tiago Ferreira <tiagoferreira@tuta.io>
2019-10-09 21:21:50 +0200

    First sketch

--- code/script.js
+++ code/script.js
@@ -9,3 +9,27 @@ function toggle() {
 document.getElementById("section1").onclick = toggle
 document.getElementById("section2").onclick = toggle
 
+function showFAQ() {
+  document.getElementById("wt").style.display = "none"
+  document.getElementById("signup-form").style.display = "none"
+  document.getElementById("faq-section").style.display = "flex"
+}
+
+function showWelcomeText() {
+  document.getElementById("faq-section").style.display = "none"
+  document.getElementById("signup-form").style.display = "none"
+  document.getElementById("wt").style.display = "flex"
+}
+
+function showSignUpForm() {
+  document.getElementById("wt").style.display = "none"
+  document.getElementById("faq-section").style.display = "none"
+  document.getElementById("signup-form").style.display = "flex"
+}
+
+document.getElementById("faq").onclick = showFAQ
+document.getElementById("close-faq").onclick = showWelcomeText
+document.getElementById("sign-up").onclick = showSignUpForm
+document.getElementById("close-signup-form").onclick = showWelcomeText
+
+
